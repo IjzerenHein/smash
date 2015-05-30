d61d70a72831efec37aa888022b44e3cd4cb97d3
IjzerenHein <hrutjes@gmail.com>
2015-05-30 09:27:38 +0200

    .. day ..

--- src/AutoLayout.js
+++ src/AutoLayout.js
@@ -10,6 +10,7 @@
 
 //var Node = require('famous/core/Node');
 var AutoLayoutParser = require('./AutoLayoutParser');
+var c = require('cassowary/bin/c');
 
 /**
  * AutoLayout
@@ -21,6 +22,75 @@ function AutoLayout(options) {
 	_parse('|-50-[background]-|');
 	_parse('[background]-70-[background2]');
 	_parse('[button(>=50)]');
+	_parse('[button(==button2)]');
+	_parse('[button(==button2,==button3)]');
+
+	/*var solver = new c.SimplexSolver();
+	var x = new c.Variable({ value: 167 });
+	var y = new c.Variable({ value: 200 });
+	var eq = new c.Equation(x, new c.Expression(y));
+	solver.addConstraint(eq);
+	solver.addEditVar(y);
+	solver.suggestValue(y, 100);
+	solver.resolve();
+	var val = x.value;
+	console.log('val: ' + val);*/
+
+	/*var solver = new c.SimplexSolver();
+
+	// Declare variables
+	var stark = new c.Variable({ value: 4 }),
+	    lannister = new c.Variable({ value: 6 }),
+	    tully = new c.Variable({ value: 2 }),
+	    frey = new c.Variable({ value: 3 });
+
+	// Declare expressions
+	var goodies = c.plus(stark, tully),
+	    baddies = c.plus(lannister, frey);
+
+	// Declare constraints
+	var wedding = new c.Inequality(baddies, c.GEQ, goodies);
+
+	// Add constraints to the solver
+	solver.add(wedding);
+
+	// Suggest values and resolve
+	solver.addEditVar(stark);
+	solver.suggestValue(stark, 100);
+	solver.resolve();
+
+	// Get a resolved value
+	var starksRemaining = stark.value;
+	console.log('starksRemaining: ' + starksRemaining);*/
+
+	var zero = new c.Variable({value: 0});
+	var width = new c.Variable({value: 100});
+	var height = new c.Variable({value: 100});
+	var solver = new c.SimplexSolver();
+	solver.addConstraint(new c.StayConstraint(zero, c.Strength.required, 0));
+	solver.addConstraint(new c.StayConstraint(width, c.Strength.required, 0));
+	solver.addConstraint(new c.StayConstraint(height, c.Strength.required, 0));
+
+	var child = {
+		left: new c.Variable(),
+		top: new c.Variable(),
+		width: new c.Variable(),
+		height: new c.Variable(),
+		right: new c.Variable(),
+		bottom: new c.Variable()
+	};
+
+	solver.addConstraint(new c.Equation(child.right, c.plus(child.left, child.width)));
+	solver.addConstraint(new c.Equation(child.bottom, c.plus(child.top, child.height)));
+
+	solver.addConstraint(new c.Equation(child.left, 10));
+	solver.addConstraint(new c.Equation(child.top, zero));
+	solver.addConstraint(new c.Equation(child.right, width));
+	solver.addConstraint(new c.Equation(child.bottom, height));
+
+	solver.resolve();
+	console.log('child: ' + JSON.stringify(child, undefined, 2));
+
 }
 
 function _parse(visualFormat) {
